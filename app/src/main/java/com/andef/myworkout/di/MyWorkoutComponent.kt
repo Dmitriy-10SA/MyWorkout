2bdb87027d3539dff64f3eab4255d9108b4272a2
dsemkin <semkin_dmitriy10@vk.com>
2025-05-06 12:20:10 +0400

    Account app

--- app/src/main/java/com/andef/myworkout/di/MyWorkoutComponent.kt
+++ app/src/main/java/com/andef/myworkout/di/MyWorkoutComponent.kt
@@ -1,6 +1,8 @@
 package com.andef.myworkout.di
 
 import android.app.Application
+import com.andef.myworkout.di.account.AccountRepositoryModule
+import com.andef.myworkout.di.account.AccountServiceModule
 import com.andef.myworkout.di.auth.AuthRepositoryModule
 import com.andef.myworkout.di.auth.AuthServiceModule
 import com.andef.myworkout.di.auth.AuthSharedPreferencesModule
@@ -9,9 +11,13 @@ import dagger.Component
 
 @Component(
     modules = [
+        //Auth
         AuthServiceModule::class,
         AuthSharedPreferencesModule::class,
-        AuthRepositoryModule::class
+        AuthRepositoryModule::class,
+        //Account
+        AccountServiceModule::class,
+        AccountRepositoryModule::class
     ]
 )
 interface MyWorkoutComponent {
