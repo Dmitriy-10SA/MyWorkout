43ee4549e72de231c59ac6a1faa285278986f1f3
dsemkin <semkin_dmitriy10@vk.com>
2025-05-10 17:10:25 +0400

    Calendar top bar ready

--- presentation/src/main/java/com/andef/myworkout/presentation/calendar/main/CalendarScreen.kt
+++ presentation/src/main/java/com/andef/myworkout/presentation/calendar/main/CalendarScreen.kt
@@ -1,15 +1,10 @@
 package com.andef.myworkout.presentation.calendar.main
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
